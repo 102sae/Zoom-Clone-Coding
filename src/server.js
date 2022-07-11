2f06cc9963a200d77a6c5101a9250f5dcac29b26
102sae <95170874+102sae@users.noreply.github.com>
2022-07-12 01:53:48 +0900

    chat

--- src/server.js
+++ src/server.js
@@ -1,11 +1,41 @@
 import express from "express";
-
+import http from "http";
+import { type } from "os";
+import { parse } from "path";
+import WebSocket from "ws";
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket); //firefox와 연결되면 넣고 chrome과 연결되면 또 추가하고
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnect from the Browser"));
+  socket.on("message", (msg) => {
+    //json.stringify는 자바스크립트 객체를 string으로 변경
+    //json.parse는 string을 자바스트립트 객체로 변경
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
